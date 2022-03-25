74cf24b1a99a3ffd49d0b88a543268ef1b5ff539
Ivan-Dell <vanbog@mail.ru>
2022-03-25 21:40:07 +0300

    homework of lesson 1

---
+++ Lesson-1 Introduction to multithreading/MVCWpfApp/Lesson-1-4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListCover<int> listCover = new();
+
+            Console.WriteLine("Programm start");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        lock (listCover)
+                        {
+                            listCover.MyAdd(j);
+                        }
+                        
+                    }
+                });
+                thread.IsBackground = true;
+                thread.Start();
+            }
+            Console.WriteLine("Program finish");
+            Console.WriteLine(listCover.MyProperty.Count);
+        }      
+        
+    }
+}
---
+++ Lesson-1 Introduction to multithreading/MVCWpfApp/Lesson-1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Threading;
+
+namespace Lesson_1
+{
+    public partial class MainWindow : Window
+    {
+        private static int _num1;
+        private static int _num2 = 1;
+        private static int _fibNum;
+        private static int _seconds = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _seconds = (int)ScrollName.Value;
+            ScrollText.Text = _seconds.ToString();
+        }
+
+        private string FibonachiNumberString(int timeout)
+        {
+            _fibNum = _num1 + _num2;
+            _num1 = _num2;
+            _num2 = _fibNum;
+
+            Thread.Sleep(timeout * 1000);
+
+            return _fibNum.ToString();
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Thread thread = new(new ThreadStart(() =>
+            {
+                string result = FibonachiNumberString(_seconds);
+                Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                {
+                    ResultText.Text = result;
+                }));
+            }));
+            thread.Start();
+        }
+
+        private void ScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            _seconds = (int)ScrollName.Track.Value;
+            ScrollText.Text = _seconds.ToString();
+        }
+
+    }
+}
