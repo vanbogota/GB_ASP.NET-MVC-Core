db5c3f94a42fe2e6c56234db6389e81cf37cb28b
Ivan-Dell <vanbog@mail.ru>
2022-03-27 23:02:46 +0300

    homework of lesson 2 part 2

--- Lesson-2-Deeping_Multithreading/Lesson-2/MyThreadPool.cs
+++ Lesson-2-Deeping_Multithreading/Lesson-2/MyThreadPool.cs
@@ -1,32 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lesson_2
+﻿namespace Lesson_2
 {
     internal class MyThreadPool
     {
         private static readonly Queue<Thread> _queue = new Queue<Thread>();
-
+        
+        public static readonly int _maxThreads = 10;
+        
         public static void AddThread(Action func)
         {
-            _queue.Enqueue(new Thread(new ThreadStart(func)));
+            if (_queue.Count > _maxThreads)
+            {
+                throw new Exception($"MyThreadPool is full. Max threads {_maxThreads}");
+            }
+            _queue.Enqueue(new Thread(new ThreadStart(func)) { IsBackground = false });
+        }
+        
+        public static void ClearMyThreadPool()
+        {
+            _queue.Clear();
         }
 
         public static void Run()
         {
-            if (_queue.Count != 0)
+            if (_queue.Count > 0)
             {
                 foreach (var thread in _queue)
                 {
-                    thread.Start();
+                    thread.Start();                    
                 }
-            }
+            }            
             else
             {
-                Console.WriteLine("ThreadPool is emty");
+               Console.WriteLine("ThreadPool is emty");
             }
         }
     }
