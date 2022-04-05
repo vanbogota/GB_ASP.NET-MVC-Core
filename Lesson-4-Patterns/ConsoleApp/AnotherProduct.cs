34b7c7a50e26a0f14337c013614c636c02ced5f2
Ivan-Dell <vanbog@mail.ru>
2022-04-06 00:21:22 +0300

    homework of lesson 4

---
+++ Lesson-4-Patterns/ConsoleApp/AnotherProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class AnotherProduct
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public long TotalAmount { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ Lesson-4-Patterns/ConsoleApp/IProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal interface IProduct
+    {
+        int Id { get; set; }
+        long Amount { get; set; }
+        string AnotherInformation { get; set; }
+    }
+}
