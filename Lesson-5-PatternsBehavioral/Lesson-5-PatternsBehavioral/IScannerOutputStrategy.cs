e973cb30fad1fba90ca952fe7bf97a57ff1d0ec8
Ivan-Dell <vanbog@mail.ru>
2022-04-10 22:37:25 +0300

    homework of lesson 5

---
+++ Lesson-5-PatternsBehavioral/Lesson-5-PatternsBehavioral/IScannerOutputStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5_PatternsBehavioral
+{
+    public interface IScannerOutputStrategy
+    {
+        void ScanAndSave(IScannerDevice scanerDevice, string output);
+    }
+}
