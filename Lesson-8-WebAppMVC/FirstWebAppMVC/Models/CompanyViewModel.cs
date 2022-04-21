c1a65cffc0fa8ad92de32ab0edc0bb30a41ebb76
Ivan-Dell <vanbog@mail.ru>
2022-04-21 15:03:22 +0300

    homework of lesson 8

---
+++ Lesson-8-WebAppMVC/FirstWebAppMVC/Models/CompanyViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstWebAppMVC.Models
+{
+    public class CompanyViewModel
+    {
+        public string CompanyName { get; set; } = null!;
+        public string? CompanyPhone { get; set; }        
+    }
+}
